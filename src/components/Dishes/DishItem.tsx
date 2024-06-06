d7f8d673da90a189060d2f3e8faf222482222d3d
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-06-06 16:23:08 +0400

    fixed class-57 two way binding

--- src/components/Dishes/DishItem.tsx
+++ src/components/Dishes/DishItem.tsx
@@ -1,19 +1,20 @@
-import {Dish} from '../../types';
 import React from 'react';
+import {Dish} from '../../types';
 
-interface Props{
-  dish:Dish
+interface Props {
+  dish: Dish;
 }
-const DishItem:React.FC<Props> = ({dish}) => {
+const DishItem: React.FC<Props> = ({dish}) => {
 
   const imageStyle = {
-    background: `url(${dish.image}) no-repeat center center / cover`
-  }
+    background: `url(${dish.image}) no-repeat center center / cover`,
+  };
+
   return (
-    <div className='card  mb-2'>
-      <div className="row g-0 gap-2">
-       <div className='col-sm-4 rounded-start' style={imageStyle}></div>
-        <div className='col-sm-8 me-2'>
+    <div className='card mb-2'>
+      <div className='row g-0'>
+        <div className='col-sm-4 rounded-start' style={imageStyle}></div>
+        <div className='col-sm-8 ps-2'>
           <h5 className='card-title'>{dish.name}</h5>
           <p className='card-text small'>{dish.description}</p>
           <p className='card-text'>{dish.price} KGS</p>
