8ec608bf97817ea402313ae317fa65995230ceb9
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-07-04 16:59:35 +0400

    added logic for cart deletion web 40

--- src/App.tsx
+++ src/App.tsx
@@ -16,67 +16,84 @@ const App = () => {
   const [dishes, setDishes] = useState<Dish[]>([
 
   ]);
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
 
   const [cartDishes, setCartDishes] = useState<CartDish[]>([
 
-  ])
+  ]);
 
   const fetchDishes = useCallback(async ()=>{
     try{
-      setLoading(true)
-      const {data:dishes} = await axiosApi.get<ApiDishesList | null>("/dishes.json")
+      setLoading(true);
+      const {data:dishes} = await axiosApi.get<ApiDishesList | null>("/dishes.json");
       if(!dishes){
-        setDishes([])
+        setDishes([]);
       }else{
         const newDishes = Object
           .keys(dishes)
           .map((id)=>({
             ...dishes[id],
             id,
-          }))
-
-        setDishes(newDishes)
+          }));
+        setDishes(newDishes);
       }
-    }catch (e){
-      throw e
     }finally {
-      setLoading(false)
+      setLoading(false);
     }
-  },[])
+  },[]);
 
 
+  const updateCard =  useCallback(async ()=>{
+    setCartDishes(cartDishes => {
+
+      return cartDishes.reduce<CartDish[]>((acc, cartDish)=>{
+        const existingDish = dishes.find(dish=> dish.id === cartDish.dish.id);
+
+
+        if(existingDish){
+          acc.push({...cartDish, dish: existingDish});
+        }
+
+        return acc;
+      },[]);
+    });
+  },[dishes]);
+
+  useEffect(() => {
+   void updateCard();
+  }, [updateCard]);
   const deleleDish = async (id:string)=>{
     if(window.confirm("are you sure you want to delete")){
       await axiosApi.delete(`/dishes/${id}.json`);
       await fetchDishes();
+
     }
   };
 
   useEffect(() => {
     if(location.pathname === "/"){
-      void fetchDishes()
+      void fetchDishes();
     }
   }, [fetchDishes, location]);
 
   const addDishToCart = (dish:Dish)=>{
      setCartDishes((prevState)=>{
        const existingIndex = prevState.findIndex(cartDish =>{
-         return cartDish.dish.id === dish.id
-       })
+         return cartDish.dish.id === dish.id;
+       });
 
        if(existingIndex === -1){
-         return[...prevState, {dish, amount: 1}]
+         return[...prevState, {dish, amount: 1}];
        }
        return prevState.map((cartDish, index)=>{
          if(index === existingIndex){
-           return {...cartDish, amount : cartDish.amount + 1}
+           return {...cartDish, amount : cartDish.amount + 1};
          }
-         return  cartDish
-       })
-     })
-  }
+         return  cartDish;
+       });
+     });
+  };
 
 
   return (
